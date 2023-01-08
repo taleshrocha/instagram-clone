10edd0d7883629e715e59d5c58bca8506c54ac18
taleshrocha <taleshrochaz@gmail.com>
2023-01-08 16:42:29 -0300

    Stories done

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -18,7 +18,8 @@ function Stories() {
   return (
     <div
       className="flex space-x-2 p-6 bg-white
-    mt-8 border-gray-200 border rounded-sm overflow-x-scroll"
+    mt-8 border-gray-200 border rounded-sm overflow-x-scroll
+    scrollbar-thin scrollbar-thumb-black"
     >
       {suggestions.map((profile) => (
         <Story
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
