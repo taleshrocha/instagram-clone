96fafa2fe2a88f40070a87ce172b002fbf29ff67
taleshrocha <taleshrochaz@gmail.com>
2023-01-08 14:16:46 -0300

    Header done

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ import { useState, useEffect } from "react";
 function Header() {
   const [popOut, setPopOut] = useState(false);
   return (
-    <div className="">
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/** Left */}
         <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
