239545c17e85f5d15a16a2044a1dae4d079c42b3
taleshrocha <taleshrochaz@gmail.com>
2023-01-09 17:11:59 -0300

    Stories show user Story

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import Story from "./Story";
 import { faker } from "@faker-js/faker";
+import { useSession } from "next-auth/react";
 
 function Stories() {
   const [suggestions, setSuggestions] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
@@ -21,6 +23,9 @@ function Stories() {
     mt-8 border-gray-200 border rounded-sm overflow-x-scroll
     scrollbar-thin scrollbar-thumb-black"
     >
+      {session && (
+        <Story img={session.user.image} userName={session.user.username} />
+      )}
       {suggestions.map((profile) => (
         <Story
           key={profile.id}
